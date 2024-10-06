25af117a8c3add079d323525a3d1eb11934efb79
Chriszosh96 <czosh1@binghamton.edu>
2024-10-06 18:01:08 -0400

    Diagnals and Text Generator

--- src/gol/gol.c
+++ src/gol/gol.c
@@ -12,42 +12,144 @@ Cell* golFetch(unsigned int x, unsigned int y) { return &golCells[y * golWidth +
 
 Cell* golFetchPrev(unsigned int x, unsigned int y) { return &golPrevCells[y * golWidth + x]; }
 
-void renderCharacter(unsigned int x, unsigned int y, char c, Cell* dest) {
+void renderCharacter(unsigned int x, unsigned int y, char c) {
     switch (c) {
     case '0':
-
+        *golFetchPrev(x, y)         = 1;
+        *golFetchPrev(x + 1, y)     = 1;
+        *golFetchPrev(x + 2, y)     = 1;
+        *golFetchPrev(x, y + 1)     = 1;
+        *golFetchPrev(x + 2, y + 1) = 1;
+        *golFetchPrev(x, y + 2)     = 1;
+        *golFetchPrev(x + 2, y + 2) = 1;
+        *golFetchPrev(x, y + 3)     = 1;
+        *golFetchPrev(x + 2, y + 3) = 1;
+        *golFetchPrev(x, y + 4)     = 1;
+        *golFetchPrev(x + 1, y + 4) = 1;
+        *golFetchPrev(x + 2, y + 4) = 1;
         break;
     case '1':
-
+        *golFetchPrev(x + 1, y)     = 1;
+        *golFetchPrev(x + 1, y + 1) = 1;
+        *golFetchPrev(x + 1, y + 2) = 1;
+        *golFetchPrev(x + 1, y + 3) = 1;
+        *golFetchPrev(x + 1, y + 4) = 1;
         break;
     case '2':
-
+        *golFetchPrev(x, y)         = 1;
+        *golFetchPrev(x + 1, y)     = 1;
+        *golFetchPrev(x + 2, y)     = 1;
+        *golFetchPrev(x + 2, y + 1) = 1;
+        *golFetchPrev(x, y + 2)     = 1;
+        *golFetchPrev(x + 1, y + 2) = 1;
+        *golFetchPrev(x + 2, y + 2) = 1;
+        *golFetchPrev(x, y + 3)     = 1;
+        *golFetchPrev(x, y + 4)     = 1;
+        *golFetchPrev(x + 1, y + 4) = 1;
+        *golFetchPrev(x + 2, y + 4) = 1;
         break;
     case '3':
-
+        *golFetchPrev(x, y)         = 1;
+        *golFetchPrev(x + 1, y)     = 1;
+        *golFetchPrev(x + 2, y)     = 1;
+        *golFetchPrev(x + 2, y + 1) = 1;
+        *golFetchPrev(x + 1, y + 2) = 1;
+        *golFetchPrev(x + 2, y + 2) = 1;
+        *golFetchPrev(x + 2, y + 3) = 1;
+        *golFetchPrev(x, y + 4)     = 1;
+        *golFetchPrev(x + 1, y + 4) = 1;
+        *golFetchPrev(x + 2, y + 4) = 1;
         break;
     case '4':
-
+        *golFetchPrev(x, y)         = 1;
+        *golFetchPrev(x + 2, y)     = 1;
+        *golFetchPrev(x, y + 1)     = 1;
+        *golFetchPrev(x + 2, y + 1) = 1;
+        *golFetchPrev(x, y + 2)     = 1;
+        *golFetchPrev(x + 1, y + 2) = 1;
+        *golFetchPrev(x + 2, y + 2) = 1;
+        *golFetchPrev(x + 2, y + 3) = 1;
+        *golFetchPrev(x + 2, y + 4) = 1;
         break;
     case '5':
-
+        *golFetchPrev(x, y)         = 1;
+        *golFetchPrev(x + 1, y)     = 1;
+        *golFetchPrev(x + 2, y)     = 1;
+        *golFetchPrev(x, y + 1)     = 1;
+        *golFetchPrev(x, y + 2)     = 1;
+        *golFetchPrev(x + 1, y + 2) = 1;
+        *golFetchPrev(x + 2, y + 2) = 1;
+        *golFetchPrev(x + 2, y + 3) = 1;
+        *golFetchPrev(x, y + 4)     = 1;
+        *golFetchPrev(x + 1, y + 4) = 1;
+        *golFetchPrev(x + 2, y + 4) = 1;
         break;
     case '6':
-
+        *golFetchPrev(x, y)         = 1;
+        *golFetchPrev(x + 1, y)     = 1;
+        *golFetchPrev(x + 2, y)     = 1;
+        *golFetchPrev(x, y + 1)     = 1;
+        *golFetchPrev(x, y + 2)     = 1;
+        *golFetchPrev(x + 1, y + 2) = 1;
+        *golFetchPrev(x + 2, y + 2) = 1;
+        *golFetchPrev(x, y + 3)     = 1;
+        *golFetchPrev(x + 2, y + 3) = 1;
+        *golFetchPrev(x, y + 4)     = 1;
+        *golFetchPrev(x + 1, y + 4) = 1;
+        *golFetchPrev(x + 2, y + 4) = 1;
         break;
     case '7':
-
+        *golFetchPrev(x, y)         = 1;
+        *golFetchPrev(x + 1, y)     = 1;
+        *golFetchPrev(x + 2, y)     = 1;
+        *golFetchPrev(x + 2, y + 1) = 1;
+        *golFetchPrev(x + 2, y + 2) = 1;
+        *golFetchPrev(x + 2, y + 3) = 1;
+        *golFetchPrev(x + 2, y + 4) = 1;
         break;
     case '8':
-
+        *golFetchPrev(x, y)         = 1;
+        *golFetchPrev(x + 1, y)     = 1;
+        *golFetchPrev(x + 2, y)     = 1;
+        *golFetchPrev(x, y + 1)     = 1;
+        *golFetchPrev(x + 2, y + 1) = 1;
+        *golFetchPrev(x, y + 2)     = 1;
+        *golFetchPrev(x + 1, y + 2) = 1;
+        *golFetchPrev(x + 2, y + 2) = 1;
+        *golFetchPrev(x, y + 3)     = 1;
+        *golFetchPrev(x + 2, y + 3) = 1;
+        *golFetchPrev(x, y + 4)     = 1;
+        *golFetchPrev(x + 1, y + 4) = 1;
+        *golFetchPrev(x + 2, y + 4) = 1;
         break;
     case '9':
-
+        *golFetchPrev(x, y)         = 1;
+        *golFetchPrev(x + 1, y)     = 1;
+        *golFetchPrev(x + 2, y)     = 1;
+        *golFetchPrev(x, y + 1)     = 1;
+        *golFetchPrev(x + 2, y + 1) = 1;
+        *golFetchPrev(x, y + 2)     = 1;
+        *golFetchPrev(x + 1, y + 2) = 1;
+        *golFetchPrev(x + 2, y + 2) = 1;
+        *golFetchPrev(x + 2, y + 3) = 1;
+        *golFetchPrev(x + 2, y + 4) = 1;
         break;
     }
 }
 
-void golInit(unsigned int width, unsigned int height, char* message) {
+void golRenderString(const char* message) {
+    // TODO - Compute offsets to center the message
+    unsigned int x_offset = (golWidth - 15) / 2;
+    unsigned int y_offset = (golHeight - 13) / 2;
+
+    for (unsigned int i = 0; i < strlen(message); ++i) {
+        int x_char_pos = x_offset + 4 * (i % 4);
+        int y_char_pos = y_offset + 7 * (i / 4);
+        renderCharacter(x_char_pos, y_char_pos, message[i]);
+    }
+}
+
+void golInit(unsigned int width, unsigned int height) {
     srand(time(NULL));
     if (golCells) {
         free(golCells);
@@ -61,14 +163,6 @@ void golInit(unsigned int width, unsigned int height, char* message) {
     memset(golPrevCells, 0, len);
     golWidth  = width;
     golHeight = height;
-
-    // TODO - Compute offsets to center the message
-    unsigned int x_offset = 0;
-    unsigned int y_offset = 0;
-
-    for (unsigned int x = 0; x < width; ++x) {
-        for (unsigned int y = 0; y < height; ++y) { *golFetchPrev(x, y) = rand() % 2; }
-    }
 }
 
 void golTick() {
@@ -76,10 +170,17 @@ void golTick() {
     for (unsigned int x = 0; x < golWidth; ++x) {
         for (unsigned int y = 0; y < golHeight; ++y) {
             int active_neighbors = 0;
-            if (y + 1 < golHeight) { active_neighbors += *golFetchPrev(x, y + 1); }
-            if (y > 0) { active_neighbors += *golFetchPrev(x, y - 1); }
-            if (x + 1 < golWidth) { active_neighbors += *golFetchPrev(x + 1, y); }
-            if (x > 0) { active_neighbors += *golFetchPrev(x - 1, y); }
+            for (int x_dir = -1; x_dir < 2; ++x_dir) {
+                if (x > 0 && x + x_dir < golWidth) {
+                    for (int y_dir = -1; y_dir < 2; ++y_dir) {
+                        if (y > 0 && y + y_dir < golHeight) {
+                            if (!(x_dir == 0 && y_dir == 0)) {
+                                active_neighbors += *golFetchPrev(x + x_dir, y + y_dir);
+                            }
+                        }
+                    }
+                }
+            }
             if (active_neighbors == 2 || active_neighbors == 3) { *golFetch(x, y) = 1; }
             else { *golFetch(x, y) = 0; }
         }
