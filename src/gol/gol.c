dce98dbb8eb841993c1a3e5a30f178265d038d5b
Ben Reid <reidben24@gmail.com>
2024-10-06 14:25:43 -0400

    Add array for prev state

--- src/gol/gol.c
+++ src/gol/gol.c
@@ -1,16 +1,23 @@
 #include <gol/gol.h>
 
 Cell* golCells         = NULL;
+Cell* golPrevCells     = NULL;
 unsigned int golWidth  = 0;
 unsigned int golHeight = 0;
 
 Cell* golFetch(unsigned int x, unsigned int y) { return &golCells[y * golWidth + x]; }
 
+Cell* golFetchPrev(unsigned int x, unsigned int y) { return &golPrevCells[y * golWidth + x]; }
+
 void golInit(unsigned int width, unsigned int height) {
-    if (golCells) { free(golCells); }
-    golCells  = malloc(width * height * sizeof(Cell));
-    golWidth  = width;
-    golHeight = height;
+    if (golCells) {
+        free(golCells);
+        free(golPrevCells);
+    }
+    golCells     = malloc(width * height * sizeof(Cell));
+    golPrevCells = malloc(width * height * sizeof(Cell));
+    golWidth     = width;
+    golHeight    = height;
 
     // TODO - think about how to init cells with text rendering & remove this
     for (unsigned int x = 0; x < width; ++x) {
@@ -22,4 +29,9 @@ void golInit(unsigned int width, unsigned int height) {
 
 void golTick() {
     // TODO - run one iteration step of GoL on golCells
+
+    // swap cells
+    Cell* tmp    = golCells;
+    golCells     = golPrevCells;
+    golPrevCells = tmp;
 }
