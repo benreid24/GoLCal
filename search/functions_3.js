3e9ec399dbb83dd15bb28098e987c392f18b7a01
benreid24 <benreid24@users.noreply.github.com>
2024-10-08 02:16:48 +0000

    deploy: 9e06b277c3c33c25651f40a22aa6ae31c03d1830

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['golfetch_88',['golFetch',['../a00014.html#af716069d3890c21ec67cdf38512677cc',1,'golFetch(unsigned int x, unsigned int y):&#160;gol.c'],['../a00023.html#af716069d3890c21ec67cdf38512677cc',1,'golFetch(unsigned int x, unsigned int y):&#160;gol.c']]],
-  ['golfetchprev_89',['golFetchPrev',['../a00014.html#a2c182d2dd8356e3fc151e7d6e89365b9',1,'golFetchPrev(unsigned int x, unsigned int y):&#160;gol.c'],['../a00023.html#a2c182d2dd8356e3fc151e7d6e89365b9',1,'golFetchPrev(unsigned int x, unsigned int y):&#160;gol.c']]],
+  ['golfetchcur_88',['golFetchCur',['../a00014.html#a78c35d9fc4f2f50dd00005959b31b21d',1,'golFetchCur(unsigned int x, unsigned int y):&#160;gol.c'],['../a00023.html#a78c35d9fc4f2f50dd00005959b31b21d',1,'golFetchCur(unsigned int x, unsigned int y):&#160;gol.c']]],
+  ['golfetchnext_89',['golFetchNext',['../a00014.html#a8339dbc6961eff7869a1e861ac558ea2',1,'golFetchNext(unsigned int x, unsigned int y):&#160;gol.c'],['../a00023.html#a8339dbc6961eff7869a1e861ac558ea2',1,'golFetchNext(unsigned int x, unsigned int y):&#160;gol.c']]],
   ['golinit_90',['golInit',['../a00014.html#aab977f0722edf915e490008f4ed8db36',1,'golInit(unsigned int width, unsigned int height):&#160;gol.c'],['../a00023.html#aab977f0722edf915e490008f4ed8db36',1,'golInit(unsigned int width, unsigned int height):&#160;gol.c']]],
   ['golrenderstring_91',['golRenderString',['../a00014.html#a91113619a2732083aa2f04fab30d6ca0',1,'golRenderString(const char *message):&#160;gol.c'],['../a00023.html#a91113619a2732083aa2f04fab30d6ca0',1,'golRenderString(const char *message):&#160;gol.c']]],
   ['goltick_92',['golTick',['../a00014.html#ad0e97ac1ef7d22d24e5510e67df05a2f',1,'golTick():&#160;gol.c'],['../a00023.html#ad0e97ac1ef7d22d24e5510e67df05a2f',1,'golTick():&#160;gol.c']]]
